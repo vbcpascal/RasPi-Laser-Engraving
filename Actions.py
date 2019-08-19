22bec70251f6b574f69f7f8ee81b61a0a572d5d3
vbcpascal <vbcpascal@gmail.com>
2019-08-19 11:41:15 +0800

    Update Actions.py

--- Actions.py
+++ Actions.py
@@ -59,4 +59,5 @@ if __name__ == "__main__":
     imr.set_mode(ir.MODE_CONTOURS)
     acts = Actions()
     acts.add_contours(imr)
+    acts.save(os.path.join('cache', 'logo.npy'))
     print(acts.top_pop())
