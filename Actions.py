22d751bab5d1c0498bcf213f51eb6e1feda9dc55
vbcpascal <vbcpascal@gmail.com>
2019-08-21 22:54:08 +0800

    add pku icon

--- Actions.py
+++ Actions.py
@@ -74,7 +74,7 @@ class Actions:
 
 if __name__ == "__main__":
     import ImageReader as ir
-    name = 'circle'
+    name = 'pku'
     imr = ir.ImageReader(os.path.join('pics', name + '.png'))
     imr.set_mode(ir.MODE_CONTOURS)
     acts = Actions()
