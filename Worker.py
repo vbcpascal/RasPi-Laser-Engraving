ffa73676cb65b0cd97a11f9e44b864c22a09baa4
vbcpascal <vbcpascal@gmail.com>
2019-08-22 20:21:20 +0800

    update pku

--- Worker.py
+++ Worker.py
@@ -91,7 +91,7 @@ class Worker:
 
         elif act_mode == ac.ACT_OPEN_LASER:
             self.laser_open()
-            self.stepper_x.wait()
+            self.stepper_x.wait(3)
 
         elif act_mode == ac.ACT_CLOSE_LASER:
             self.laser_close()
