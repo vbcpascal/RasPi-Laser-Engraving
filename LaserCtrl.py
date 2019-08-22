f1e7b428791d38d5b52a347fe9d537e2c3c58e74
vbcpascal <vbcpascal@gmail.com>
2019-08-22 20:38:10 +0800

    Update LaserCtrl.py

--- LaserCtrl.py
+++ LaserCtrl.py
@@ -32,7 +32,7 @@ class LaserCtrl(object):
         self.status = CLOSED
 
     def open(self):
-        self.__change_duty_cycle(100)
+        self.__change_duty_cycle(85)
         self.status = OPENED
 
     def close(self):
